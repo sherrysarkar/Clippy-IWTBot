0947b40e901f7cbf8610b32584e02095b1d99dd5
sherrysarkar <sherry.sarkar@outlook.com>
2018-10-20 21:01:01 -0400

    Graphing

--- datasetTest.py
+++ datasetTest.py
@@ -1,5 +1,7 @@
 import requests
 import json
+import matplotlib
+import matplotlib.pyplot as plt
 
 auth_data = {
     "grant_type"    : "client_credentials",
@@ -29,10 +31,10 @@ reference_request_url = 'https://api.marquee.gs.com/v1/assets/data/query'
 
 request_query = {
                     "where": {
-                        "gsid": ["901237","11308","177256"]
+                        "gsid": ["901237"]
                     },
                     "startDate": "2017-01-15",
-                    "endDate":"2018-01-15"
+                    "endDate":"2018-01-15",
                }
 
 reference_query = {
@@ -44,15 +46,47 @@ reference_query = {
 }
 
 
-
-coverage = session.get(url=coverage_request_url)
-coverage_data = json.loads(coverage.text)
-print(coverage_data)
+#coverage = session.get(url=coverage_request_url)
+#coverage_data = json.loads(coverage.text)
+#print(coverage_data)
 
 reference = session.post(url=reference_request_url, json=reference_query)
 reference_data = json.loads(reference.text)
-print(reference_data)
+#print(reference_data)
 
-# request = session.post(url=request_url, json=request_query)
-# results = json.loads(request.text)
-# print(results)
+request = session.post(url=request_url, json=request_query)
+results = json.loads(request.text)
+print(results["data"][1])
+
+###### Graphing #######
+
+# 'growthScore': 0.234, 'multipleScore': 0.076, 'gsid': '901237', 'financialReturnsScore': 0.71,'c': 0.624
+
+data = results["data"]
+
+growthScore = []
+multipleScore = []
+finReturnScore = []
+intergratedScore = []
+X = []
+
+for j, entry in enumerate(data):
+    growthScore.append(entry['growthScore'])
+    multipleScore.append(entry['multipleScore'])
+    finReturnScore.append(entry['financialReturnsScore'])
+    intergratedScore.append(entry['integratedScore'])
+    X.append(j)
+
+fig, ax = plt.subplots()
+plt.plot(X, growthScore, color="green")
+plt.plot(X, multipleScore, color="blue")
+#plt.plot(X, finReturnScore, color="red")
+#plt.plot(X, intergratedScore, color="black")
+
+
+ax.set(xlabel='time (days)', ylabel='Scores',
+       title='Different Scores')
+ax.grid()
+
+plt.show()
+
